eea312eeaad77e9b52b5dc3ae0bc46ee2e28e4eb
ddunlop <ddunlop@gmail.com>
2010-07-08 02:12:15 -0400

    removed log files

--- application/logs/2010-07-01.log.php
+++
@@ -1,4 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2010-07-01 23:30:28 -04:00 --- error: Uncaught Kohana_Exception: The requested view, analytics, could not be found in file system/core/Kohana.php on line 1162
-2010-07-01 23:40:09 -04:00 --- error: Uncaught PHP Error: Missing argument 2 for html_Core::link(), called in /opt/local/apache2/htdocs/snack/application/views/codewars/index.php on line 2 and defined in file system/helpers/html.php on line 303
