07013e0cb2e3fa32475b719b1060ae868dcd2a69
Aman <amanahuja@gmail.com>
2010-10-11 14:45:28 -0700

    Setting up commits.

--- application/views/codewars/index.php
+++ application/views/codewars/index.php
@@ -1,5 +1,5 @@
 <?php echo $header ?>
-This is the main body - explain what codewars is here and entice the person to login/register
+This is the main body - explain what Code-Wars is here and entice the person to login/register
 <ul>
 <li><?php echo html::anchor('/login','Login') ?></li>
 <li><?php echo html::anchor('/register','Register') ?></li>
