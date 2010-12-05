81bf13e0a3b23e93f71e84ad3304f9b9d9a95fc4
Aman <amanahuja@gmail.com>
2010-12-04 18:46:58 -0800

    Cleaned up style and content on index and sidebar-basic

--- application/views/codewars/index.php
+++ application/views/codewars/index.php
@@ -6,24 +6,34 @@ echo View::factory('common/header')
 <!-- Main body goes here -->
 <div id="content" class="home">
 <h2 class="punch">Code-wars is a social community centered around algorithms and coding. Individuals and teams compete to solve problems in the form of games and competitions. Contestants are scored and ranked by their ability to outperform others.</h2>
-<p style="margin:5px 0;">Code-wars is different from other online coding challenges.</p>
+
+<h2>Code-wars is different from other online coding challenges.</h2>
 
 <div class="yui-g">
 	<div class="yui-u first">
-		<h4>Infrastructure</h2>
-		Use any language or computing environment<br />
-		Work alone or in a team<br />
-		Your code never leaves your computer<br />
+		<h4>Flexible Infrastructure</h2>
+		<div class="checkbox"><ul>
+			<li>Use any language or computing environment</li>
+			<li>Work alone or in a team</li>
+			<li>Your code never leaves your computer</li>
+		</ul></div>
 	</div>
 	<div class="yui-u">
-		<h4>Challenges</h4>
-		All competition take place in real time<br />
-		Innovation and experimentation encouraged<br />
-		Advanced challenges that have no perfect solution<br />
+		<h4>Smarter Challenges</h4>
+		<div class="checkbox"><ul>
+		<li>All competitions take place in real time</li>
+		<li>Designed to encourage innovation</li>
+		<li>No one solution is best</li>
+		</ul></div>
 	</div>
 </div>
+
+<h2>Get Started</h2>
 <p style="margin:5px 0;">Use our Start Wizard to get going, or learn more about our goals</p>
 
+<h2>Learn more</h2>
+	<p>You can find out more about how Code-Wars works and what we're all about by reading the FAQ and browsing the community forums.	</p>
+	
 <?php 
 /*
 <p>Give it a shot! 
--- application/views/common/sidebar-basic.php
+++ application/views/common/sidebar-basic.php
@@ -1,28 +1,33 @@
-<!-- Move this to a Sidebar view?  -->
-<div id="sidebar" class="yui-b">
-	<!-- sidebar content goes here -->
+
+<div class="yui-b">
+<div id="sidebar"> 
 	<div class="widget">
-	<h4><?php echo html::anchor('gettingstarted','Getting started') ?></h4>
+	<h2>Get Started</h2>
 	<ul>
-	<?php
+		<li><?php echo html::anchor('gettingstarted','...with our easy-start wizard') ?></li>
+	<?php /*
 		echo '<li>',html::anchor('gettingstarted#register','Register'),'</li>';
 		echo '<li>',html::anchor('gettingstarted#pick','Pick a Challenge'),'</li>';
 		echo '<li>',html::anchor('gettingstarted#download','Download a Starter Bot'),'</li>';
 		echo '<li>',html::anchor('gettingstarted#connect','Connect and Compete!'),'</li>';
-	?>
+	*/ ?>
+	
 	</ul>
 	</div>
 	
-	<h4>Games and Challenges</h4>
-	<div id="game-1" class="game"><h5>Connect 4</h5></div>
-	<div id="game-2" class="game"><h5>Tanks</h5></div>
-	<div id="game-3" class="game"><h5>Undercut</h5></div>
-	<div id="game-4" class="game"><h5>EEF Challenge</h5></div>
+	<h2>Games and Challenges</h2>
+	<div id="game-1" class="game"><h3>Connect 4</h3>
+		<p>This classic game of four-in-a-row is a great introduction to Code-wars and to game programming.</p>
+	</div>
+	<div id="game-2" class="game"><h3>Tanks</h3>
+		<p>Fire your tank at the target and get scored on your accuracy. Built for a class on differential equations. </p>
+	</div>
+	<div id="game-3" class="game"><h3>Undercut</h3>
+		<p>Not as simple as it seems. Try to "out-psych" your opponent in this Hofsatdterian psychological game. </p>	
+	</div>
+	<div id="game-4" class="game"><h3>EEF Challenge <span style="font-variant:small-caps;">(private)</span></h3>
+		<p>This new challenge will be open to all after the private competition for University of Colorado at Boulder has concluded. Stay tuned!</p>
+	</div>
 	
-	<div class="widget" style="margin-top:15px;">
-	<h4>Learn more</h4>
-	<p>You can find out more about how Code-Wars works and what we're all about by reading the FAQ and browsing the community forums.	</p>
-	</div>  <!-- .widget -->
-	
-</div> <!-- .yui-b for sidebar -->
+</div></div> <!-- .yui-b for sidebar -->
 
