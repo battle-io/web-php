645432521778d58a655a61c8934d930f49cd7c52
James <james@Rukia.(none)>
2010-12-14 06:18:37 +0800

    Began adding the bullshit pages to the server.

---
+++ application/views/bullshit/wrappers.php
@@ -0,0 +1,15 @@
+<div id="content" class="home">
+  <h2>C++</h2>
+  <a href="">C++ Wrapper</a>
+  <br/>
+  <h2>Java</h2>
+  <a href="">Java Wrapper</a>
+  <br/>
+  <h2>Matlab</h2>
+  <a href="">Matlab Wrapper</a>
+  <br/>
+  <h2>Python</h2>
+  <a href="">Python Wrapper</a>
+</div>
+</div>
+</div>
