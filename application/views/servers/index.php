bb2ea1b0e416550fe2669ef0098468fb7064c7e1
Aman <amanahuja@gmail.com>
2011-01-15 17:27:50 -0800

    Converted Servers list to table (aesthetic). Requires Challenge type to be fetched.

--- application/views/servers/index.php
+++ application/views/servers/index.php
@@ -3,21 +3,35 @@ echo View::factory('common/header')
         ->set('title','Game Servers');
 ?>
 <h3>Game Servers</h3>
-<h4>This is the Servers Page - Lists all the servers</h4>
-<?php
-	echo '<ul>';
-	foreach($servers as $server) {
-		echo '<li>',
-			html::anchor($server->uri(),$server->name),
-			' ',
-			$server->online?'Online':'Offline',
-			'<ul>',
-				'<li>Number of Bots ',$server->bots->count_all(),'</li>',
-			'</ul>';
-	}
-	echo '</ul>';
-?>
+<h4>List of active Game Servers</h4>
 
+<div id="servertable">
+<div class="yui-gc tableheader">
+	<div class="yui-g first">
+		<div class="yui-u first">Server Name</div>
+		<div class="yui-u">Challenge</div>
+	</div>
+	<div class="yui-g">
+		<div class="yui-u first">Status</div>
+		<div class="yui-u">Registered Bots</div>
+	</div>
+</div>
+
+<?php
+	echo '<div class="yui-gc">';
+	foreach($servers as $server) {
+		echo '<div class="yui-g first"><div class="yui-u first">',
+			html::anchor($server->uri(),$server->name),
+			'</div><div class="yui-u">(Bullshit)</div></div>',
+			'<div class="yui-g"><div class="yui-u first">',
+			$server->online?'Online':'Offline',
+			'</div><div class="yui-u center">',
+				$server->bots->count_all(),
+			'</div></div>';
+	}
+	echo '</div>';
+?>
+</div>
 <?php
 echo View::factory('common/footer');
 //echo View::factory('profiler/stats');
